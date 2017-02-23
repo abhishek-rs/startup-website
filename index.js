099bd79c57d4e8b54c614dedefb8117d43299eae
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-02-23 21:42:40 +0200

    added team

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
     var $nav_bar = $(".navbar");
     var $nav_bar_brand = $("#brand li a");
     var $nav_bar_logo = $(".navbar-logo");
+    var $who_are_we = $("#who-are-we .filler");
+    var $how_it_works = $("#where .filler");
+    var $how_it_works2 = $("#how-it-works .filler");
+    var $team = $("#team .filler")
+    var $fixed = $(".fixed");
     var top = 200;
     var flag = false;
 
@@ -20,25 +25,47 @@ $(document).ready(function() {
                 $nav_bar_brand.addClass('fixedfont');
 
                 $nav_bar_logo.addClass('fixedlogo');
+
             //    $nav_bar_logo.attr('src','logoblack.png');
                 //  $(".button").css("color":"white");
-            } else if (flag == true) {
-                $nav_bar.addClass('fixed');
-                $nav_bar_brand.addClass('fixedfont');
-                $nav_bar_logo.addClass('fixedlogo');
-          //      $nav_bar_logo.attr('src','logoblack.png');
+                if( y >= $who_are_we.offset().top && y < $how_it_works.offset().top){
+                  $nav_bar.css('background-color', $who_are_we.css('background-color'));
+                  //console.log(y, $nav_bar.css('background-color'));
+                }
+                else if (y>=$how_it_works.offset().top && y < $how_it_works2.offset().top) {
+                  $nav_bar.css('background-color', $how_it_works.css('background-color'));
+                }
+                else if (y>=$how_it_works2.offset().top && y < $team.offset().top){
+                  $nav_bar.css('background-color', $how_it_works2.css('background-color'));
+                }
+                else if (y>=$team.offset().top ){
+                  $nav_bar.css('background-color', $team.css('background-color'));
+                }
+
+                else {
+                  $nav_bar.css('background-color', '#334D5C');
+                }
+
+            }
+
+            else {
+               // otherwise remove it
+               $nav_bar.css('background-color', "");
+               $nav_bar.removeClass('fixed');
+               $nav_bar_brand.removeClass('fixedfont');
+
+               $nav_bar_logo.removeClass('fixedlogo');
+           //    $nav_bar_logo.attr('src','logowhite.png');
+
+               //   $(".button").css("color":"#81BEF7");
+           }
+
+
 
                 //  $(".button").css("color":"white");
-            } else {
-                // otherwise remove it
-                $nav_bar.removeClass('fixed');
-                $nav_bar_brand.removeClass('fixedfont');
 
-                $nav_bar_logo.removeClass('fixedlogo');
-            //    $nav_bar_logo.attr('src','logowhite.png');
 
-                //   $(".button").css("color":"#81BEF7");
-            }
+
         });
         return false;
     }
